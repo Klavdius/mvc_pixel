64356280b8b04eaaa4a59ad48ed4f8124d6a0159
Vyacheslav <Oblom.91@list.ru>
2021-08-10 14:17:49 +0300

    first commit

--- application/core/Route.php
+++ application/core/Route.php
@@ -12,14 +12,15 @@ class Route
         //check for an empty url
         $sep = explode('/',$newUrl);
         if(empty($sep[1])){
-           $list = file_get_contents('src\list\main.html',true);
+           $list = file_get_contents('src/code/main.php',true);
         return true;
         }
-        //build main root
-            $rootUrl = ('src/list/' . "$newUrl" . '.html');
-            $rootUrl=trim($rootUrl," ");
-         //check file in system
+
+        $rootUrl = Tools::DecodeUrl($newUrl);
+           $rootUrl = trim($rootUrl, " ");
+
         if(!(file_exists("$rootUrl")) ){
+            echo $rootUrl . " по этому пути ищем " . br;
             $list = file_get_contents('src\list\error.html',true);
             echo $list;
             return false;
@@ -27,6 +28,11 @@ class Route
         //output file
             $list = file_get_contents("$rootUrl",true);
             echo $list;
+
+        if($rootUrl == 'src/code/main.php') {
+            Foreman::build();
+        }
             return true;
     }
 }
+
